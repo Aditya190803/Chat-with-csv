eb57f244a6417c6d8b031d69dd967c7e307bd092
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-05-13 14:30:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
-from pandasai.llm.local_llm import LocalLLM
-import streamlit as st 
-import pandas as pd 
-from pandasai import SmartDataframe
+import pandas as pd
 import os
+import streamlit as st
+from pandasai import SmartDataframe
 from langchain_groq.chat_models import ChatGroq
-from dotenv import load_dotenv
+from pandasai.responses.response_parser import ResponseParser
 
-load_dotenv(override=True)
+llm = ChatGroq(model_name="llama3-70b-8192",api_key = os.environ["GROQ_API_KEY"])
 
-model = ChatGroq(model_name="llama3-70b-8192",api_key = os.environ["GROQ_API_KEY"])
-#model = LocalLLM(api_base="http://localhost:11434/v1",model="llama2")
+def load_data(uploaded_file) -> pd.DataFrame:
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        return data
+    return None
 
-st.title("Data analysis with PandasAI")
+class StreamlitResponse(ResponseParser):
+    def format_dataframe(self, result):
+        st.dataframe(result["value"])
 
-uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
+    def format_plot(self, result):
+        st.image(result["value"])
+
+    def format_other(self, result):
+        st.write(result["value"])
+
+st.write("# Chat with CSV Data 🦙")
+
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write(data.head(3))
+    df = load_data(uploaded_file)
 
-    df = SmartDataframe(data, config={"llm": model})
-    prompt = st.text_area("Enter your prompt:")
+    with st.expander("🔎 Dataframe Preview"):
+        st.write(df.tail(3))
 
-    if st.button("Generate"):
-        if prompt:
-            with st.spinner("Generating response..."):
-                st.write(df.chat(prompt))
+    query = st.text_area("🗣️ Chat with Dataframe")
+    container = st.container()
 
+    if query:
+        
+        query_engine = SmartDataframe(
+            df,
+            config={
+                "llm": llm,
+                "response_parser": StreamlitResponse,
+            },
+        )
+
+        answer = query_engine.chat(query)
